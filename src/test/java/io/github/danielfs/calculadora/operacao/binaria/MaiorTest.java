470ef23f1b8c3f41509fe88b3b2225172c49a3b7
sandromatias <sandro@unipam.edu.br>
2014-11-26 22:11:09 -0200

    Create MaiorTest.java

---
+++ src/test/java/io/github/danielfs/calculadora/operacao/binaria/MaiorTest.java
@@ -0,0 +1,24 @@
+package io.github.danielfs.calculadora.operacao.binaria;
+
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+
+public class MaiorTest{
+  private Maior maior;
+  
+@Before
+public void inicializar(){
+  maior = new Maior();
+}
+
+@Test
+public void deveriaRetornar3QuandoPassa3e2(){
+double valorEsperado = 3;
+double valorRetornado = maior.calcular(3,2);
+
+assertEquals(valorEsperado, valorRetornado,0);
+
+}
+
+}
